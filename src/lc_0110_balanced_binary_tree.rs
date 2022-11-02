42d917ce316f057f80224c24d03398beca81583b
herschel-ma <maliang_gs@126.com>
2022-11-02 17:26:44 +0800

    lc 0110 balanced binary tree

---
+++ src/lc_0110_balanced_binary_tree.rs
@@ -0,0 +1,47 @@
+use crate::TreeNode;
+use std::{cell::RefCell, rc::Rc};
+
+pub fn is_balanced(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+    height_is_balanced(&root).is_some()
+}
+
+fn height_is_balanced(root: &Option<Rc<RefCell<TreeNode>>>) -> Option<i32> {
+    if let Some(root) = root {
+        let lh = height_is_balanced(&root.borrow().left);
+        let rh = height_is_balanced(&root.borrow().right);
+        match (lh, rh) {
+            (Some(lh), Some(rh)) => {
+                if (lh - rh).abs() < 2 {
+                    return Some(lh.max(rh) + 1);
+                } else {
+                    None
+                }
+            }
+            _ => None,
+        }
+    } else {
+        Some(0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn ex1() {
+        let tree = TreeNode::from_vec(&[3, 9, 20, i32::MIN, i32::MIN, 15, 7]);
+        assert!(is_balanced(tree));
+    }
+
+    #[test]
+    fn ex2() {
+        let tree = TreeNode::from_vec(&[1, 2, 2, 3, 3, i32::MIN, i32::MIN, 4, 4]);
+        assert!(!is_balanced(tree));
+    }
+
+    #[test]
+    fn ex3() {
+        let tree = TreeNode::from_vec(&[]);
+        assert!(is_balanced(tree));
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,14 @@ pub use lc_0001_two_sums::*;
 pub mod lc_0020_valid_parentheses;
 pub use lc_0020_valid_parentheses::*;
 
-
 pub mod lc_0021_merge_two_sorted_lists;
 pub use lc_0021_merge_two_sorted_lists::*;
 
+pub mod lc_0110_balanced_binary_tree;
+pub use lc_0110_balanced_binary_tree::*;
+
 pub mod lc_0121_best_time_to_buy_and_sell_stock;
 pub use lc_0121_best_time_to_buy_and_sell_stock::*;
-
 
 pub mod lc_0125_valid_palindrome;
 pub use lc_0125_valid_palindrome::*;
