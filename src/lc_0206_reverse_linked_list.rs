af1c71e4a1ac75cfd6713873c75cf7d30a9d42d1
herschel-ma <maliang_gs@126.com>
2022-11-11 22:12:53 +0800

    lc 0206 reverse linked list

---
+++ src/lc_0206_reverse_linked_list.rs
@@ -0,0 +1,47 @@
+use crate::ListNode;
+pub struct Solution;
+
+impl Solution {
+    pub fn reverse_list(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut ary = vec![];
+        while let Some(node) = head {
+            ary.push(node.val);
+            head = node.next;
+        }
+        let mut ptr = None;
+        for val in ary {
+            let mut node = ListNode::new(val);
+            node.next = ptr;
+            ptr = Some(Box::new(node));
+        }
+        ptr
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ex1() {
+        let head = ListNode::from_vec(&vec![1, 2, 3, 4, 5]);
+        assert_eq!(
+            vec![5, 4, 3, 2, 1],
+            Solution::reverse_list(head).unwrap().as_ref().into_array()
+        )
+    }
+
+    #[test]
+    fn ex2() {
+        let head = ListNode::from_vec(&vec![1, 2]);
+        assert_eq!(
+            vec![2, 1],
+            Solution::reverse_list(head).unwrap().as_ref().into_array()
+        )
+    }
+
+    #[test]
+    fn ex3() {
+        assert!(Solution::reverse_list(None).is_none())
+    }
+}
