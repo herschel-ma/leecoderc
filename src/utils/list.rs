9754c1360447b1767da46a4d821dd9c70f797557
herschel-ma <maliang_gs@126.com>
2022-11-11 21:49:27 +0800

    link list add to_array

--- src/utils/list.rs
+++ src/utils/list.rs
@@ -1,38 +1,47 @@
 // Definition for singly-linked list.
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
-  pub val: i32,
-  pub next: Option<Box<ListNode>>
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
 }
 
 impl ListNode {
-  #[inline]
-  pub fn new(val: i32) -> Self {
-    ListNode {
-      next: None,
-      val
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        ListNode { next: None, val }
     }
-  }
 
-  pub fn from_vec(vec: &[i32]) -> Option<Box<ListNode>> {
-    let mut result = None;
-    for i in vec.iter().rev() {   
-        let mut node = Self::new(*i);
-        node.next = result;
-        // println!("i = {i:?}, node={node:?}");
-        result = Some(Box::new(node));
+    pub fn from_vec(vec: &[i32]) -> Option<Box<ListNode>> {
+        let mut result = None;
+        for i in vec.iter().rev() {
+            let mut node = Self::new(*i);
+            node.next = result;
+            // println!("i = {i:?}, node={node:?}");
+            result = Some(Box::new(node));
+        }
+        result
     }
-    result 
-  }
+
+    pub fn into_array(&self) -> Vec<i32> {
+        let mut res = vec![self.val];
+        let mut head = &self.next;
+        while let Some(node) = head {
+            res.push(node.val);
+            head = &node.next;
+        }
+        res
+    }
 }
 
-
+#[cfg(test)]
 mod tests {
+    use std::vec;
+
     use super::*;
 
     #[test]
     pub fn creat_list() {
-        let mut list = ListNode::from_vec(&vec![1,2,3,4,5]);
+        let mut list = ListNode::from_vec(&vec![1, 2, 3, 4, 5]);
 
         let mut n = 1;
         while let Some(entry) = list {
@@ -40,5 +49,12 @@ mod tests {
             n += 1;
             list = entry.next
         }
+    }
+
+    #[test]
+    fn convert_back_to_array() {
+        let list = ListNode::from_vec(&vec![1, 2, 3, 4, 5]).unwrap();
+        let ary = list.into_array();
+        assert_eq!(ary, vec![1, 2, 3, 4, 5])
     }
 }
