1cf9752aa7b393bffeec89bf2f4e9fd2f9eef018
herschel-ma <maliang_gs@126.com>
2023-10-22 11:05:34 +0800

    quick sort 2

--- src/quick_sort.rs
+++ src/quick_sort.rs
@@ -1,16 +1,18 @@
 pub fn quick_sort(mut array: Vec<i32>) -> Vec<i32> {
-    let mut res = Vec::with_capacity(array.len());
     // 基线条件：为空或只包含一个元素的数组是“有序”的
     if array.len() < 2 {
         return array;
     } else {
-        let pivot = array.remove(0);
+        let pivot = array.remove(0); // 递归条件
+        // 前者为由所有小于基准值的元素组成的子数组
+        // 后者为由所有大于基准值的元素组成的子数组
         let (less, greater): (Vec<i32>, Vec<i32>) = array.into_iter().partition(|&x| x <= pivot);
-        res.append(&mut quick_sort(less));
-        res.append(&mut vec![pivot]);
-        res.append(&mut quick_sort(greater));
+        let mut sorted_less = quick_sort(less);
+        let sorted_greater = quick_sort(greater);
+        sorted_less.push(pivot);
+        sorted_less.extend(sorted_greater);
+        return sorted_less
     }
-    res
 }
 
 #[cfg(test)]
