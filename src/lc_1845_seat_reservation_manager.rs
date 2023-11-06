d9f1ba10e5e0adc095b1ae853f74dd063d62b8f3
herschel-ma <maliang_gs@126.com>
2023-11-06 13:16:24 +0800

    lc 1845 seat reservation manager

---
+++ src/lc_1845_seat_reservation_manager.rs
@@ -0,0 +1,28 @@
+struct SeatManager {
+    seats: Vec<i32>,
+}
+
+impl SeatManager {
+    fn new(n: i32) -> Self {
+        Self {
+            seats: (1..n).rev().collect(),
+        }
+    }
+    fn reserve(&mut self) -> i32 {
+        self.seats.pop().unwrap()
+    }
+    fn unreserve(&mut self, seat_number: i32){
+        let mut left: usize = 0;
+        let mut right: usize = self.seats.len();
+
+        while left < right {
+            let middle: usize = left + (right - left) / 2;
+            if self.seats[middle] < seat_number {
+                right = middle;
+            } else {
+                left = middle + 1;
+            }
+        }
+        self.seats.insert(left, seat_number);
+    }
+}
