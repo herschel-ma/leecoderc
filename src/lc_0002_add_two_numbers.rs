d067bc47699754274298f22c7420fcf2842614e2
herschel-ma <maliang_gs@126.com>
2023-11-13 15:44:21 +0800

    lc  0002 add two numbers

---
+++ src/lc_0002_add_two_numbers.rs
@@ -0,0 +1,66 @@
+use crate::ListNode;
+
+pub fn add_two_numbers(
+    l1: Option<Box<ListNode>>,
+    l2: Option<Box<ListNode>>,
+) -> Option<Box<ListNode>> {
+    let mut carray = 0;
+    let mut l1 = l1;
+    let mut l2 = l2;
+    let mut dummy = ListNode::new(-1);
+    let mut p = &mut dummy;
+    while l1.is_some() || l2.is_some() {
+        let mut sum = carray;
+        if let Some(node) = l1 {
+            sum += node.val;
+            l1 = node.next;
+        }
+        if let Some(node) = l2 {
+            sum += node.val;
+            l2 = node.next;
+        }
+        carray = sum / 10;
+        p.next = Some(Box::new(ListNode {
+            val: sum % 10,
+            next: None,
+        }));
+        p = p.next.as_mut().unwrap();
+    }
+    if carray != 0 {
+        p.next = Some(Box::new(ListNode {
+            val: carray,
+            next: None,
+        }));
+    }
+    dummy.next
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_case1() {
+        let l1 = ListNode::from_vec(&[2, 4, 3]);
+        let l2 = ListNode::from_vec(&[5, 6, 4]);
+        let output = vec![7, 0, 8];
+        assert_eq!(add_two_numbers(l1, l2), ListNode::from_vec(&output));
+    }
+
+    #[test]
+    fn test_case2() {
+        let l1 = ListNode::from_vec(&[0]);
+        let l2 = ListNode::from_vec(&[0]);
+        let output = vec![0];
+        assert_eq!(add_two_numbers(l1, l2), ListNode::from_vec(&output));
+    }
+
+    #[test]
+    fn test_case3() {
+        let l1 = ListNode::from_vec(&[9, 9, 9, 9, 9, 9, 9]);
+        let l2 = ListNode::from_vec(&[9, 9, 9, 9]);
+        let output = vec![8, 9, 9, 9, 0, 0, 0, 1];
+        assert_eq!(add_two_numbers(l1, l2), ListNode::from_vec(&output));
+    }
+}
+
