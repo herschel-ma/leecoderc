65053fe50e86e5cffa7e570553a34c0eef22bd99
herschel-ma <maliang_gs@126.com>
2023-11-13 18:02:11 +0800

    lc 0002 add two numbers recursive version

--- src/lc_0002_add_two_numbers.rs
+++ src/lc_0002_add_two_numbers.rs
@@ -1,6 +1,6 @@
 use crate::ListNode;
 
-pub fn add_two_numbers(
+pub fn add_two_numbers_v1(
     l1: Option<Box<ListNode>>,
     l2: Option<Box<ListNode>>,
 ) -> Option<Box<ListNode>> {
@@ -35,6 +35,30 @@ pub fn add_two_numbers(
     dummy.next
 }
 
+pub fn add_two_numbers(
+    l1: Option<Box<ListNode>>,
+    l2: Option<Box<ListNode>>,
+) -> Option<Box<ListNode>> {
+    match (l1, l2) {
+        (None, None) => None,
+        (Some(n), None) | (None, Some(n)) => Some(n),
+        (Some(n1), Some(n2)) => {
+            let sum = n1.val + n2.val;
+            if sum < 10 {
+                Some(Box::new(ListNode {
+                    val: sum,
+                    next: add_two_numbers(n1.next, n2.next),
+                }))
+            } else {
+                let carry = Some(Box::new(ListNode::new(1)));
+                Some(Box::new(ListNode {
+                    val: sum - 10,
+                    next: add_two_numbers(add_two_numbers(carry, n1.next), n2.next),
+                }))
+            }
+        }
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
