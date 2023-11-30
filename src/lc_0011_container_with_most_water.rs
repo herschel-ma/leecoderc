8c94b869aaa0908a92ee54f1ad2ae9833c8c95e6
herschel-ma <maliang_gs@126.com>
2023-11-30 15:51:34 +0800

    lc 11 container with most water

---
+++ src/lc_0011_container_with_most_water.rs
@@ -0,0 +1,34 @@
+pub fn max_area(height: Vec<i32>) -> i32 {
+    let mut i = 0;
+    let mut j = height.len() - 1;
+    let mut res = 0;
+    while i < j {
+        res = res.max(height[i].min(height[j]) * (j - i) as i32);
+        if height[i] <= height[j] {
+            i += 1;
+        } else {
+            j -= 1;
+        }
+    }
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_case1() {
+        let height = vec![1, 8, 6, 2, 5, 4, 8, 3, 7];
+        let res = 49;
+        assert_eq!(max_area(height), res);
+    }
+
+    #[test]
+    fn test_case2() {
+        let height = vec![1, 1];
+        let res = 1;
+        assert_eq!(max_area(height), res);
+    }
+}
+
