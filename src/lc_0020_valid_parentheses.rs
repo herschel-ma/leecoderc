ee92c5168789f64a7e8230074ad7b5ad121f7aa1
herschel-ma <maliang_gs@126.com>
2023-12-01 09:54:59 +0800

    lc 0020 valid parentheses

--- src/lc_0020_valid_parentheses.rs
+++ src/lc_0020_valid_parentheses.rs
@@ -1,4 +1,6 @@
-pub fn is_valid(s: String) -> bool {
+use std::collections::HashMap;
+
+pub fn is_valid_1(s: String) -> bool {
     let mut stack = "".to_string();
     for ch in s.chars() {
         match ch {
@@ -15,6 +17,23 @@ pub fn is_valid(s: String) -> bool {
                 }
             }
             _ => return false,
+        }
+    }
+    stack.is_empty()
+}
+
+pub fn is_valid(s: String) -> bool {
+    let mut map = HashMap::new();
+    map.insert('(', ')');
+    map.insert('[', ']');
+    map.insert('{', '}');
+
+    let mut stack = vec![];
+    for ch in s.chars() {
+        if map.contains_key(&ch) {
+            stack.push(map[&ch]);
+        } else if stack.pop().unwrap_or('-') != ch {
+            return false;
         }
     }
     stack.is_empty()
