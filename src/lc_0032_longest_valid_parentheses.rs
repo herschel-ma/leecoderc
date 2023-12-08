63a5d2bf3ab18ae7f045ce8573f4d087dda8f231
herschel-ma <maliang_gs@126.com>
2023-12-08 15:20:55 +0800

    lc 0032 longest valid parentheses dynamic programming

--- src/lc_0032_longest_valid_parentheses.rs
+++ src/lc_0032_longest_valid_parentheses.rs
@@ -1,4 +1,5 @@
 pub fn longest_valid_parentheses(s: String) -> i32 {
+pub fn longest_valid_parentheses_stack(s: String) -> i32 {
     let mut stack = vec![-1];
     let mut res = 0;
     for i in 0..s.len() {
@@ -14,6 +15,31 @@ pub fn longest_valid_parentheses(s: String) -> i32 {
         }
     }
     res
+}
+
+pub fn longest_valid_parentheses(s: String) -> i32 {
+    // ()(())
+    let mut max_length = 0;
+    let mut dp = vec![0; s.len()];
+    for i in 1..s.len() {
+        if s.chars().nth(i).unwrap() == ')' {
+            if s.chars().nth(i - 1).unwrap() == '(' {
+                dp[i] = if i >= 2 { dp[i - 2] + 2 } else { 2 };
+            } else if i as i32 - dp[i - 1] > 0
+                && s.chars().nth(i - dp[i - 1] as usize - 1).unwrap() == '('
+            {
+                dp[i] = dp[i - 1]
+                    + 2
+                    + if i - dp[i - 1] as usize >= 2 {
+                        dp[i - dp[i - 1] as usize - 2]
+                    } else {
+                        0
+                    };
+            }
+            max_length = max_length.max(dp[i]);
+        }
+    }
+    max_length
 }
 
 #[cfg(test)]
