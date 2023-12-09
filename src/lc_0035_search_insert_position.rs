7b1bc8e7296b5a89fac3c8084aa7f07576046491
herschel-ma <maliang_gs@126.com>
2023-12-09 17:18:17 +0800

    lc 0035 search insert position

---
+++ src/lc_0035_search_insert_position.rs
@@ -0,0 +1,54 @@
+pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+    let n = nums.len();
+    let mut left = 0;
+    let mut right = n;
+    while left < right {
+        let mid = (left + right) >> 1;
+        if nums[mid] < target {
+            left = mid + 1;
+        } else if nums[mid] > target {
+            right = mid;
+        } else {
+            return mid as i32;
+        }
+    }
+    left as i32
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_case_1() {
+        let nums = vec![1, 3, 5, 6];
+        let target = 5;
+        let output = 2;
+        assert_eq!(search_insert(nums, target), output)
+    }
+
+    #[test]
+    fn test_case_2() {
+        let nums = vec![1, 3, 5, 6];
+        let target = 2;
+        let output = 1;
+        assert_eq!(search_insert(nums, target), output)
+    }
+
+    #[test]
+    fn test_case_3() {
+        let nums = vec![1, 3, 5, 6];
+        let target = 7;
+        let output = 4;
+        assert_eq!(search_insert(nums, target), output)
+    }
+
+    #[test]
+    fn test_case_4() {
+        let nums = vec![1, 3, 5, 6];
+        let target = 0;
+        let output = 0;
+        assert_eq!(search_insert(nums, target), output)
+    }
+}
+
