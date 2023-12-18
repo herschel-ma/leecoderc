511cc8a47b8edc5395b47d6cf1e20828e136be2a
herschel-ma <maliang_gs@126.com>
2023-12-18 14:06:05 +0800

    lc 0067 add binary solution 2

--- src/lc_0067_add_binary.rs
+++ src/lc_0067_add_binary.rs
@@ -1,63 +1,87 @@
-pub fn add_binary(a: String, b: String) -> String {
-    let a: Vec<i32> = a
-        .chars()
-        .rev()
-        .map(|c| match c {
-            '0' => 0,
-            '1' => 1,
-            _ => panic!("oops"),
-        })
-        .collect();
-    let b: Vec<i32> = b
-        .chars()
-        .rev()
-        .map(|c| match c {
-            '0' => 0,
-            '1' => 1,
-            _ => panic!("oops"),
-        })
-        .collect();
+use crate::Solution;
+use std::cmp::Ordering;
 
-    let mut i = 0;
-    let mut carry = 0;
-    let mut result = Vec::new();
-    println!("{a:?}-{b:?}");
-    while i < a.len() && i < b.len() {
-        let sum = a[i] + b[i] + carry;
-        result.push(sum & 1);
-        carry = (sum & 2) >> 1;
-        i += 1;
-        println!("{sum:?}-{carry:?}-{result:?}")
-    }
-    while i < a.len() {
-        let sum = a[i] + carry;
-        result.push(sum & 1);
-        carry = (sum & 2) >> 1;
-        i += 1;
-        println!("{sum:?}-{carry:?}-{result:?}")
-    }
-    while i < b.len() {
-        let sum = b[i] + carry;
-        result.push(sum & 1);
-        carry = (sum & 2) >> 1;
-        i += 1;
-        println!("{sum:?}-{carry:?}-{result:?}")
-    }
+impl Solution {
+    pub fn add_binary_1(a: String, b: String) -> String {
+        let a: Vec<i32> = a
+            .chars()
+            .rev()
+            .map(|c| match c {
+                '0' => 0,
+                '1' => 1,
+                _ => panic!("oops"),
+            })
+            .collect();
+        let b: Vec<i32> = b
+            .chars()
+            .rev()
+            .map(|c| match c {
+                '0' => 0,
+                '1' => 1,
+                _ => panic!("oops"),
+            })
+            .collect();
 
-    if carry == 1 {
-        result.push(carry);
-    }
-
-    let mut answer = "".to_string();
-    while let Some(x) = result.pop() {
-        if x == 1 {
-            answer.push('1');
-        } else {
-            answer.push('0');
+        let mut i = 0;
+        let mut carry = 0;
+        let mut result = Vec::new();
+        while i < a.len() && i < b.len() {
+            let sum = a[i] + b[i] + carry;
+            result.push(sum & 1);
+            carry = (sum & 2) >> 1;
+            i += 1;
         }
+        while i < a.len() {
+            let sum = a[i] + carry;
+            result.push(sum & 1);
+            carry = (sum & 2) >> 1;
+            i += 1;
+        }
+        while i < b.len() {
+            let sum = b[i] + carry;
+            result.push(sum & 1);
+            carry = (sum & 2) >> 1;
+            i += 1;
+        }
+
+        if carry == 1 {
+            result.push(carry);
+        }
+
+        let mut answer = "".to_string();
+        while let Some(x) = result.pop() {
+            if x == 1 {
+                answer.push('1');
+            } else {
+                answer.push('0');
+            }
+        }
+
+        answer
     }
 
-    answer
+    pub fn add_binary(a: String, b: String) -> String {
+        let mut i = (a.len() as i32) - 1;
+        let mut j = (b.len() as i32) - 1;
+        let mut carry = 0;
+        let mut ans = String::new();
+        let a = a.as_bytes();
+        let b = b.as_bytes();
+
+        while i >= 0 || j >= 0 || carry > 0 {
+            if i >= 0 {
+                carry += a[i as usize] - b'0';
+                i -= 1;
+            }
+            if j >= 0 {
+                carry += b[j as usize] - b'0';
+                j -= 1;
+            }
+            ans.push_str(&(carry & 1).to_string());
+            carry >>= 1;
+        }
+        ans.chars().rev().collect()
+    }
 }
 
 #[cfg(test)]
@@ -67,7 +91,7 @@ mod test {
     #[test]
     fn ex1() {
         assert_eq!(
-            add_binary("11".to_string(), "1".to_string()),
+            Solution::add_binary("11".to_string(), "1".to_string()),
             "100".to_string()
         );
     }
@@ -75,8 +99,30 @@ mod test {
     #[test]
     fn ex2() {
         assert_eq!(
-            add_binary("1010".to_string(), "1011".to_string()),
+            Solution::add_binary("1010".to_string(), "1011".to_string()),
             "10101".to_string()
+        );
+    }
+
+    #[test]
+    fn ex3() {
+        assert_eq!(
+            Solution::add_binary("0".to_string(), "0".to_string()),
+            "0".to_string()
+        );
+    }
+    #[test]
+    fn ex4() {
+        assert_eq!(
+            Solution::add_binary("1".to_string(), "111".to_string()),
+            "1000".to_string()
+        );
+    }
+    #[test]
+    fn ex5() {
+        assert_eq!(
+            Solution::add_binary("1111".to_string(), "1111".to_string()),
+            "11110".to_string()
         );
     }
 }
