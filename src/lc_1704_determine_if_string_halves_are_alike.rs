c1ec3131b0f2706d6dd58a466915647e768177e3
herschel-ma <maliang_gs@126.com>
2024-01-12 11:01:37 +0800

    lc 1704 determine if string halves are alike

---
+++ src/lc_1704_determine_if_string_halves_are_alike.rs
@@ -0,0 +1,44 @@
+use crate::Solution;
+
+impl Solution {
+    pub fn halves_are_alike(s: String) -> bool {
+        fn is_vowel(c: char) -> bool {
+            let vowels = "aeiouAEIOU";
+            vowels.contains(c)
+        }
+
+        let s = s.split_at(s.len() / 2);
+
+        s.0.chars().filter(|&c| is_vowel(c)).count().eq(&s
+            .1
+            .chars()
+            .filter(|&c| is_vowel(c))
+            .count())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::ops::Not;
+
+    use super::*;
+
+    #[test]
+    fn test_case_1() {
+        let s = String::from("book");
+        assert!(Solution::halves_are_alike(s));
+    }
+
+    #[test]
+    fn test_case_2() {
+        let s = String::from("textbook");
+        assert!(Solution::halves_are_alike(s).not());
+    }
+
+    #[test]
+    fn test_case_3() {
+        let s = String::from("AbCdEfGh");
+        assert!(Solution::halves_are_alike(s));
+    }
+}
+
