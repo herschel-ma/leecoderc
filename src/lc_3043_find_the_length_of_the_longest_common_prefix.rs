39898da3acfe4843de3e7ef34d7be40cc33778a9
herschel-ma <maliang_gs@126.com>
2024-09-29 11:02:52 +0800

    lc 3043: add another solution while implemented by trie

--- src/lc_3043_find_the_length_of_the_longest_common_prefix.rs
+++ src/lc_3043_find_the_length_of_the_longest_common_prefix.rs
@@ -1,7 +1,56 @@
 use crate::Solution;
 
+#[derive(Default, Debug, Clone)]
+struct TrieNode {
+    // another way to implement children: HashMap<char, TrieNode>
+    children: [Option<Box<TrieNode>>; 10],
+}
+
+#[derive(Default, Debug)]
+pub struct Trie {
+    root: TrieNode,
+}
+
+impl Trie {
+    fn new() -> Self {
+        Trie {
+            root: TrieNode::default(),
+        }
+    }
+
+    fn insert(&mut self, num: &i32) {
+        let mut node = &mut self.root;
+        let num_str = num.to_string();
+        for c in num_str.chars() {
+            if node.children[c.to_digit(10).unwrap() as usize].is_none() {
+                node.children[c.to_digit(10).unwrap() as usize] =
+                    Some(Box::new(TrieNode::default()));
+            }
+            node = node.children[c.to_digit(10).unwrap() as usize]
+                .as_mut()
+                .unwrap()
+        }
+    }
+
+    fn find_longest_common_prefix(&self, num: &i32) -> i32 {
+        let mut node = &self.root;
+        let num_str = num.to_string();
+        let mut length = 0;
+        for c in num_str.chars() {
+            match &node.children[c.to_digit(10).unwrap() as usize] {
+                Some(no) => {
+                    node = no;
+                    length += 1;
+                }
+                None => break,
+            }
+        }
+        length
+    }
+}
+
 impl Solution {
-    pub fn longest_common_prefix(arr1: Vec<i32>, arr2: Vec<i32>) -> i32 {
+    pub fn longest_common_prefix_1(arr1: Vec<i32>, arr2: Vec<i32>) -> i32 {
         //step1: build a hashset to store all the possible prefix in arr1
         let mut arr1_prefixes = std::collections::HashSet::new();
         for mut a1 in arr1.into_iter() {
@@ -23,23 +72,42 @@ impl Solution {
         }
         longest_prefix
     }
+
+    pub fn longest_common_prefix_2(arr1: Vec<i32>, arr2: Vec<i32>) -> i32 {
+        // create a trie to store prefix
+        let mut trie = Trie::new();
+        for num in arr1 {
+            trie.insert(&num);
+        }
+        let mut ans = 0;
+        for num in arr2 {
+            ans = ans.max(trie.find_longest_common_prefix(&num));
+        }
+
+        ans
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    type Fn = fn(arr1: Vec<i32>, arr2: Vec<i32>) -> i32;
+    const S1: Fn = Solution::longest_common_prefix_1;
+    const S2: Fn = Solution::longest_common_prefix_2;
 
     #[test]
     fn test_case_1() {
         let arr1 = vec![1, 10, 100];
         let arr2 = vec![1000];
-        assert_eq!(Solution::longest_common_prefix(arr1, arr2), 3);
+        assert_eq!(S1(arr1.clone(), arr2.clone()), 3);
+        assert_eq!(S2(arr1, arr2), 3);
     }
 
     #[test]
     fn test_case_2() {
         let arr1 = vec![1, 2, 3];
         let arr2 = vec![4, 4, 4];
-        assert_eq!(Solution::longest_common_prefix(arr1, arr2), 0);
+        assert_eq!(S1(arr1.clone(), arr2.clone()), 0);
+        assert_eq!(S2(arr1, arr2), 0);
     }
 }
